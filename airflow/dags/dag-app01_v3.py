17f6023cd857b59f571d684f90d01fd6b3c0a7af
Thiago Villani <iamthiagovillani@gmail.com>
2024-11-16 22:43:38 -0300

    Add files via upload

---
+++ airflow/dags/dag-app01_v3.py
@@ -0,0 +1,47 @@
+from datetime import timedelta, datetime
+from airflow import DAG
+# Operators; we need this to operate!
+from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
+from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import SparkKubernetesSensor
+from airflow.utils.dates import days_ago
+
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': days_ago(1),
+    'email': ['teste_teste.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'max_active_runs': 1,
+    'retries': 1
+}
+
+dag = DAG(
+    'Test-App-Spark_v3',
+    default_args=default_args,
+    schedule_interval=timedelta(days=1),
+    tags=['sparkapplication'],
+)
+
+submit = SparkKubernetesOperator(
+    task_id='spark_transform_data',
+    namespace="default",
+    image= 'rodrigosf05/data-platform-open-source:spark-3.4.4_V1',
+    code_path="local:///opt/App/my-app-test.py",
+    application_file="sparkoperator-app01.yaml",
+    kubernetes_conn_id="k8s",
+    do_xcom_push=True,
+    dag=dag
+)
+
+sensor = SparkKubernetesSensor(
+    task_id='spark_app_monitor',
+    namespace="sampletenant",
+    application_name="{{ task_instance.xcom_pull(task_ids='spark_pi_submit')['metadata']['name'] }}",
+    kubernetes_conn_id="k8s",
+    dag=dag,
+    attach_log=True
+)
+
+submit >> sensor
